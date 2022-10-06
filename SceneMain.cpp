a89dc83b7522f269a781a797ad0e4a0e24b9a47e
Hmmtrnt <rento5310@gmail.com>
2022-10-06 15:21:58 +0900

    サウンド追加

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -25,11 +25,36 @@ SceneBase* SceneMain::update()
 		m_textVecX = -4;
 	}
 
-	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	if (padState & PAD_INPUT_2)
+	if (CheckHitKey(KEY_INPUT_A))
 	{
-		PlaySoundFile("sound/cursor1.mp3", DX_PLAYTYPE_NORMAL);
+		PlaySoundFile("sound/cursor0.mp3", DX_PLAYTYPE_BACK);
 	}
+	if (CheckHitKey(KEY_INPUT_S))
+	{
+		PlaySoundFile("sound/cursor1.mp3", DX_PLAYTYPE_BACK);
+	}
+	if (CheckHitKey(KEY_INPUT_D))
+	{
+		PlaySoundFile("sound/cursor2.mp3", DX_PLAYTYPE_BACK);
+	}
+	if (CheckHitKey(KEY_INPUT_F))
+	{
+		PlaySoundFile("sound/cursor3.mp3", DX_PLAYTYPE_BACK);
+	}
+	if (CheckHitKey(KEY_INPUT_G))
+	{
+		PlaySoundFile("sound/cursor4.mp3", DX_PLAYTYPE_BACK);
+	}
+	if (CheckHitKey(KEY_INPUT_H))
+	{
+		PlaySoundFile("sound/cursor5.mp3", DX_PLAYTYPE_BACK);
+	}
+	if (CheckHitKey(KEY_INPUT_J))
+	{
+		PlaySoundFile("sound/cursor6.mp3", DX_PLAYTYPE_BACK);
+	}
+	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+
 	if (padState & PAD_INPUT_3)
 	{
 		return (new SceneTitle);
